968e848790e5b8e939b512fb603b22f3e5591401
lts-pj <lliamsymonds04@gmail.com>
2024-12-29 19:06:24 +1000

    using new hand tracker

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import keyboard
+import cv2
 
 from HandTracker import HandTracker
 from MouseController import MouseController
@@ -7,19 +8,20 @@ from MouseController import MouseController
 if __name__ == "__main__":
     pass
 
-    hand_tracker = HandTracker(model_path='Models\\hand_landmarker.task')
+    hand_tracker = HandTracker(model_path='Models\\hand_landmarker.task', debug_mode=True)
     mouse_controller = MouseController()
 
     prev_time = time.time()
     while True:
         now = time.time()
-        dt = now - prev_time
+        dt = max(now - prev_time, 1/100)
 
         hand_tracker.update(dt)
-        mouse_controller.update(hand_tracker.velocity, dt) #will need to scale this by delta time
+        mouse_controller.update(hand_tracker.wrist_tracker, dt) #will need to scale this by delta time
 
         prev_time = now
 
-        if keyboard.is_pressed('`'):
-            print("exiting...`")
+        if cv2.waitKey(1) & 0xFF == 27: #esc key
+            print("exiting...")
+            hand_tracker.close()
             break
