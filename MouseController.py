24f7fa116974a646a1c7c90809ce95df37fe8699
lts-pj <lliamsymonds04@gmail.com>
2024-12-29 19:05:46 +1000

    fixed mouse jitter

--- MouseController.py
+++ MouseController.py
@@ -1,13 +1,34 @@
-
+import math
 import time
 import mouse
+import pyautogui
 
-VELOCITY_SCALAR = 500
+VELOCITY_SCALAR_X = 0.05
+VELOCITY_SCALAR_Y = 0.1
+MIN_MOVEMENT_THRESHOLD = 0.05
+
+JUMP_THRESHOLD = 0.5
+JUMP_MULTIPLIER = 2
+
+from Spring import Spring
+from PointTracker import PointTracker
+
+def calculate_magnitude(v: (float, float)) -> float:
+    return math.sqrt(pow(v[0], 2) + pow(v[1], 2))
+
+def log_curve(x: float, a: float):
+    return math.copysign(math.log(abs(x) + 1) + 1, x) * a
 
 class MouseController:
     def __init__(self):
         pass
 
-    def update(self, velocity: (float, float), dt: float):
-        # pyautogui.moveRel(int(velocity[0] * VELOCITY_SCALAR), int(velocity[1] * VELOCITY_SCALAR),
-        mouse.move(int(velocity[0] * VELOCITY_SCALAR), int(velocity[1] * VELOCITY_SCALAR), absolute=False)
+    def update(self, index_finger_tracker: PointTracker, dt: float):
+        displacement_magnitude = calculate_magnitude(index_finger_tracker.displacement)
+
+        if displacement_magnitude > 0.001:
+            screen_width, screen_height = pyautogui.size()
+
+            x = int(index_finger_tracker.displacement[0] * screen_width)
+            y = int(index_finger_tracker.displacement[1] * screen_height)
+            mouse.move(x, y, False)
