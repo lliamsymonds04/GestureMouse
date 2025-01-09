9ba82a917cf3f69a3b6ed19d8c39433865567814
lts-pj <lliamsymonds04@gmail.com>
2025-01-09 14:54:56 +1000

    correctly picks correct webcam source

--- HandTracker.py
+++ HandTracker.py
@@ -16,7 +16,15 @@ HAND_TO_TRACK = "Right"
 
 class HandTracker:
     def __init__(self, model_path: str, debug_mode: bool = False):
-        self.cap = cv2.VideoCapture(0)
+        # self.cap = cv2.VideoCapture(0)
+        for i in range(3):
+            cap = cv2.VideoCapture(i)
+            ret = cap.read()
+            if ret[0]:
+                print(f"Camera {i} selected")
+                self.cap = cap
+                break
+
 
         self.start_time = time.time()
         self.previous_time = self.start_time
@@ -71,12 +79,15 @@ class HandTracker:
             return
 
         mp_image = mediapipe.Image(image_format=mediapipe.ImageFormat.SRGB, data=numpy.array(frame))
+        mp_image_np = numpy.array(mp_image.numpy_view())
 
         # Call the async detection method
         now = time.time()
         if not self.processing_hand:
             self.processing_hand = True
             self.landmarker.detect_async(mp_image, timestamp_ms=int((now - self.start_time) * 1000))
+
+        height, width, depth = frame.shape
 
         detected_hand = False
         if self.landmark_result is not None:
@@ -92,13 +103,13 @@ class HandTracker:
                     detected_hand = True
 
             elif len(result.handedness) == 2:
-                x_positions = list()
+                x_positions = [0,0]
                 for handedness in result.handedness:
                     hand = handedness[0]
 
                     hand_landmarks = result.hand_landmarks[hand.index]
                     marker = hand_landmarks[0]
-                    x_positions.append(marker.x)
+                    x_positions[hand.index] = marker.x
 
                 hand_index = 0
                 if HAND_TO_TRACK == "Right":
@@ -108,6 +119,9 @@ class HandTracker:
 
                 landmarks = result.hand_landmarks[hand_index]
                 detected_hand = True
+
+            if self.debug_mode:
+                cv2.putText(mp_image_np, f"hands detected: {len(result.handedness)}", (300, int(height * 0.9)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
 
             if landmarks is not None:
@@ -124,8 +138,8 @@ class HandTracker:
         self.previous_time = now
         self.can_see_hand = detected_hand
 
+
         if self.debug_mode:
-            mp_image_np = numpy.array(mp_image.numpy_view())
 
             if detected_hand:
                 height, width, depth = frame.shape
@@ -134,8 +148,6 @@ class HandTracker:
                     x,y = int(tracker.x  * width), int(tracker.y * height)
 
                     cv2.circle(mp_image_np, (x, y), 10, (255, 0, 0), 2)
-
-            # cv2.imshow(DEBUG_WINDOW_NAME, mp_image_np)
 
         return mp_image_np
 
